5f5126a081d033ff93acb2cefc3a839ad38762c4
duy1009 <duy10092001@gmail.com>
2024-07-03 14:05:38 +0700

    #update mode openfile

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,14 @@
 
 class LogCSV():
-    def __init__(self, path:str, header:list):
+    def __init__(self, path:str, header:list, mode:str='w'):
         self.data = []
         self.path = path
         self.header = header
+        self.mode = mode
     
     def save(self):
         txt = ", ".join(self.header) + "\n"
-        f = open(self.path, "w")
+        f = open(self.path, self.mode)
         for val in self.data:
             v_str = [str(i) if type(i) is not str else i for i in val]
             txt+= ", ".join(v_str) + "\n"
