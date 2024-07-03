73c4654f791bbfd67eb1b3790121080984dd541f
duy1009 <duy10092001@gmail.com>
2024-07-03 14:34:27 +0700

    #fix logger mode a

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,13 @@
-
+import os.path as osp
 class LogCSV():
     def __init__(self, path:str, header:list, mode:str='w'):
         self.data = []
         self.path = path
         self.header = header
         self.mode = mode
+        if mode == "a" and not osp.exists(path):
+            self.update_a(header)
+
     
     def save(self):
         txt = ", ".join(self.header) + "\n"
@@ -17,3 +20,10 @@ class LogCSV():
 
     def update(self, values:list):
         self.data.append(values)
+
+    def update_a(self, value):
+        f = open(self.path, self.mode)
+        v_str = [str(i) if type(i) is not str else i for i in value]
+        txt= ", ".join(v_str) + "\n"
+        f.write(txt)
+        f.close()
