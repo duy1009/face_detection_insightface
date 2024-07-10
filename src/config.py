648599e31b7273ae3b505ee1b7a69a4ade397136
duy1009 <duy10092001@gmail.com>
2024-07-10 17:33:26 +0700

    #update configs show

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ PORT = '8000'
 LOG_DIR = "log" 
 MODEL_PATH = "weights/det_10g.onnx"
 # SRC = "/home/hungdv/tcgroup/Jetson/insightface/face_detection_insightface/src/IMG_5272.MOV"
-SRC = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)3264, height=(int)2464, framerate=(fraction)21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink drop=True"
+SRC = "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)3264, height=(int)2464, framerate=(fraction)21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)3264, height=(int)3264, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink drop=True"
 
 # Config time
 TIME_REQUEST_NSTRANGER= 10  
@@ -17,7 +17,6 @@ RESET_LOG = True
 DET_INPUT_SIZE = (640, 640)
 DET_CONF_FIL = 0.5
 FACE_ANGLE_FIL = 180
-SHOW_DETECT = True
 
 # Config tracking
 TRACK_MAX_HIDE_FRAME = 10
@@ -26,7 +25,6 @@ SCORE_RATE = 0.5
 # Config recognize
 IMG_SIZE_REQUEST_MIN = 50
 THREAD_REQUEST_MAX = 10
-SHOW_RECOGNIZE = True
 
 # Log sync
 TIME_SYNC = 3
@@ -36,3 +34,8 @@ TEMPURATURE_WARM = 50000
 TEMPURATURE_HOT = 80000
 WARM_DELAY_TIME = 100
 NORMAL_DELAY_TIME = 50
+
+# Show
+IMG_SIZE_SHOW = (1000, 800)
+SHOW_RECOGNIZE = True
+SHOW_DETECT = True
